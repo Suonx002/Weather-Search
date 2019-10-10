9188614116274e8bc53a104f8c51760b2b34089d
Vuthy Suon <Suonx002@gmail.com>
2019-10-09 22:46:05 -0500

    fixing issues with user input lowercase and uppercase

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 const cityInput = document.querySelector('#city-search');
 const stateInput = document.querySelector('#state-search');
 const weatherForm = document.querySelector('.weather-form');
-const weatherContainer = document.querySelector('.weather-container');
 const weatherResults = document.querySelector('.weather-results');
-// 2895f439a5e9484f82efcf679f045bd9
-
-// weatherBtn.addEventListener('click', () => {
-//   fetch(
-//     `https://api.weatherbit.io/v2.0/current?city=Shakopee,MN&key=2895f439a5e9484f82efcf679f045bd9`
-//   )
-//     .then(res => res.json())
-//     .then(data => data.data[0])
-//     .then(data => console.log(data));
-// });
 
 weatherForm.addEventListener('submit', getData);
 
 function getData(e) {
   const api_key = '2895f439a5e9484f82efcf679f045bd9';
+  const city =
+    cityInput.value.slice(0, 1).toUpperCase() + cityInput.value.slice(1);
+  const state = stateInput.value.toUpperCase();
   fetch(
-    `https://api.weatherbit.io/v2.0/current?city=Shakopee,MN&key=${api_key}`
+    `https://api.weatherbit.io/v2.0/current?city=${city},${state}&key=${api_key}`
   )
     .then(response => response.json())
     .then(data => data.data[0])
     .then(weather => {
       displayWeather(weather);
+      clearInput();
     })
     .catch(err => {
       alert('There is something wrong with the data...');
@@ -35,7 +28,10 @@ function getData(e) {
 
 function displayWeather(weather) {
   const fahrenheit = ((weather.temp * 9) / 5 + 32).toFixed(1);
-  console.log(weather);
+
+  //clear results
+  weatherResults.innerHTML = '';
+
   let output = `
     <h4 class="city">${weather.city_name}, <span class="state">${weather.state_code}</span></h4>
     <p class="timezone"><strong>Time Zone:</strong> ${weather.timezone}</p>
@@ -52,4 +48,9 @@ function displayWeather(weather) {
   `;
   weatherResults.insertAdjacentHTML('beforeend', output);
   weatherResults.style.display = 'block';
+}
+
+function clearInput() {
+  cityInput.value = '';
+  stateInput.value = '';
 }
